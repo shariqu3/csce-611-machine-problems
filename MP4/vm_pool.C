9a327185ae60e099be8b1a6e1ea15b03eac188e4
Sharique <sharique@tamu.edu>
2026-03-18 17:47:39 -0500

    Moved region maintenance arrays to frame pool

--- MP4/vm_pool.C
+++ MP4/vm_pool.C
@@ -31,7 +31,7 @@
 /* CONSTANTS */
 /*--------------------------------------------------------------------------*/
 
-/* -- (none) -- */
+#define MAX_REGIONS 256
 
 /*--------------------------------------------------------------------------*/
 /* FORWARDS */
@@ -50,19 +50,35 @@ VMPool::VMPool(unsigned long _base_address, unsigned long _size,
   frame_pool = _frame_pool;
   page_table = _page_table;
 
+  allocated = (Region *)base_address;
+  free_regions = (Region *)(base_address + MAX_REGIONS * sizeof(Region));
+
   int n_alloc = 0;
   int n_free = 1;
 
   free_regions[0].base = base_address;
   // Chose to round down here if requested memory is not a multiple of page size
-  free_regions[0].n_pages = _size / _page_table->PAGE_SIZE;
+  free_regions[0].n_pages = _size / page_table->PAGE_SIZE;
 
   page_table->register_pool(this);
   Console::puts("Constructed VMPool object.\n");
 }
 
 unsigned long VMPool::allocate(unsigned long _size) {
-  assert(false);
+  // Rounding up here
+  // unsigned long req_pages =
+  //     (_size + page_table->PAGE_SIZE - 1) / page_table->PAGE_SIZE;
+  // if (n_alloc >= MAX_REGIONS) {
+  //   Console::puts("Out of memory regions.\n");
+  //   return 0;
+  // }
+  //
+  // for (int i = 0; i < n_free; i++) {
+  //   if (free_regions[i].n_pages >= req_pages) {
+  //     // allocated[n_alloc].base = free_regions
+  //   }
+  // }
+  // return 0;
   Console::puts("Allocated region of memory.\n");
 }
 
