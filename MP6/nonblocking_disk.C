31c868abcf420292ab4b2cde6a92d23ef5564ed0
Sharique <sharique@tamu.edu>
2026-04-12 22:05:23 -0500

    Completed bonus options-1&2

--- MP6/nonblocking_disk.C
+++ MP6/nonblocking_disk.C
@@ -32,21 +32,103 @@
 
 NonBlockingDisk::NonBlockingDisk(unsigned int _size) 
   : SimpleDisk(_size) {
-    waiting_thread = nullptr;
+    start = 0;
+    n_threads = 0;
+    is_disk_in_use = false;
+    current_disk_thread = nullptr;
+    for (int i = 0; i < 8; i++) {
+        disk_queue[i] = nullptr;
+    }
     InterruptHandler::register_handler(14, this);
+}
+
+bool NonBlockingDisk::possible_disk_operation() {
+    return !is_disk_in_use && (n_threads < 8);
 }
 
 void NonBlockingDisk::wait_while_busy() {
     while (is_busy()) {
-        waiting_thread = Thread::CurrentThread();
+        current_disk_thread = Thread::CurrentThread();
         System::SCHEDULER->yield();
     }
 }
 
+void NonBlockingDisk::read(unsigned long _block_no, unsigned char* _buf) {
+    bool is_ints_enabled = Machine::interrupts_enabled();
+    if (is_ints_enabled) Machine::disable_interrupts();
+    
+    while (is_disk_in_use) {
+        if (n_threads < 8) {
+            disk_queue[(start + n_threads++) % 8] = Thread::CurrentThread();
+        } else {
+            Console::puts("Disk queue full!\n");
+            assert(false);
+        }
+        
+        if (is_ints_enabled) Machine::enable_interrupts();
+        System::SCHEDULER->yield(); // Wait to be dequeued
+        if (is_ints_enabled) Machine::disable_interrupts();
+    }
+    
+    is_disk_in_use = true;
+    if (is_ints_enabled) Machine::enable_interrupts();
+
+    // Actual operation
+    SimpleDisk::read(_block_no, _buf);
+
+    if (is_ints_enabled) Machine::disable_interrupts();
+    is_disk_in_use = false;
+    
+    if (n_threads > 0) {
+        Thread* next = disk_queue[start];
+        disk_queue[start] = nullptr;
+        start = (start + 1) % 8;
+        n_threads--;
+        System::SCHEDULER->resume(next);
+    }
+    if (is_ints_enabled) Machine::enable_interrupts();
+}
+
+void NonBlockingDisk::write(unsigned long _block_no, unsigned char* _buf) {
+    bool is_ints_enabled = Machine::interrupts_enabled();
+    if (is_ints_enabled) Machine::disable_interrupts();
+    
+    while (is_disk_in_use) {
+        if (n_threads < 8) {
+            disk_queue[(start + n_threads++) % 8] = Thread::CurrentThread();
+        } else {
+            Console::puts("Disk queue full!\n");
+            assert(false);
+        }
+        
+        if (is_ints_enabled) Machine::enable_interrupts();
+        System::SCHEDULER->yield(); // Wait to be dequeued
+        if (is_ints_enabled) Machine::disable_interrupts();
+    }
+    
+    is_disk_in_use = true;
+    if (is_ints_enabled) Machine::enable_interrupts();
+
+    // Actual operation
+    SimpleDisk::write(_block_no, _buf);
+
+    if (is_ints_enabled) Machine::disable_interrupts();
+    is_disk_in_use = false;
+    
+    if (n_threads > 0) {
+        Thread* next = disk_queue[start];
+        disk_queue[start] = nullptr;
+        start = (start + 1) % 8;
+        n_threads--;
+        System::SCHEDULER->resume(next);
+    }
+    if (is_ints_enabled) Machine::enable_interrupts();
+}
+
 void NonBlockingDisk::handle_interrupt(REGS * _r) {
-    if (waiting_thread != nullptr) {
-        Thread* t = waiting_thread;
-        waiting_thread = nullptr;
+    if (current_disk_thread != nullptr) {
+        Thread* t = current_disk_thread;
+        current_disk_thread = nullptr;
         System::SCHEDULER->resume(t);
     }
 }
