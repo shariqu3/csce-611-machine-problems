a5f322bf6dc86274f1388993f912dc90524c7915
Sharique <sharique@tamu.edu>
2026-05-03 18:50:36 -0500

    Implemented FileSystem::Format

--- MP7/file_system.C
+++ MP7/file_system.C
@@ -38,9 +38,11 @@
 /*--------------------------------------------------------------------------*/
 
 FileSystem::FileSystem() {
-    Console::puts("In file system constructor.\n");
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    // Console::puts("In file system constructor.\n");
+    // Console::puts("FUNCTION NOT IMPLEMENTED\n");
+    // assert(false);
+    disk = NULL;
+    inodes = new Inode[MAX_INODES];
 }
 
 FileSystem::~FileSystem() {
@@ -59,17 +61,57 @@ FileSystem::~FileSystem() {
 bool FileSystem::Mount(SimpleDisk * _disk) {
     Console::puts("mounting file system from disk\n");
     /* Here you read the inode list and the free list into memory */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    /*
+    1. Check if disk is NULL, if not return false
+    2. Initialize  disk, n_blocks & size
+    3. Read to memory inodes and free_blocks
+    End: return true
+    */
+
+    // 1. Check if disk is NULL, if not return false
+    if(disk != NULL)
+        return false;
+
+    // 2. Initialize  disk, n_blocks & size
+    disk = _disk;
+    size = MAX_INODES * SimpleDisk::BLOCK_SIZE;
+    n_blocks = MAX_INODES;
+
+    // 3. Read to memory inodes and free_blocks
+    free_blocks = new unsigned char[n_blocks];
+    disk->read(1, free_blocks); 
+
+    unsigned char* buf = new unsigned char[SimpleDisk::BLOCK_SIZE/sizeof(unsigned char)];
+    disk->read(0, buf); 
+    Console::puti(int(free_blocks[0]));
+
+    delete[](buf);
+    return true;
 }
 
 bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) { // static!
     Console::puts("formatting disk\n");
+
     /* Here you populate the disk with an initialized (probably empty) inode list
        and a free list. Make sure that blocks used for the inodes and for the free list
        are marked as used, otherwise they may get overwritten. */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    // Console::puts("FUNCTION NOT IMPLEMENTED\n");
+    // assert(false);
+    /*
+    1. Overwrite inodes block
+    2. Overwrite free_block
+    */
+
+    // 1. Overwrite inodes block
+    unsigned char* buf = new unsigned char[SimpleDisk::BLOCK_SIZE/sizeof(unsigned char)];
+    _disk->write(0, buf);
+
+    // 2. Overwrite free_block
+    buf[0] = 1;
+    buf[1] = 1;
+    _disk->write(1, buf);
+    delete[] buf;
+    return true;
 }
 
 Inode * FileSystem::LookupFile(int _file_id) {
